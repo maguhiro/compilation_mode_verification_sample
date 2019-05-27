6b86fc6d92331dffe8d924bc63457229c59516ae
maguhiro <maguhiro@gmail.com>
2019-05-27 23:30:12 +0900

    add compilation mode sample codes

---
+++ Sample/SuperViewController.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+class SuperViewController: UIViewController, UITableViewDelegate {
+}
--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -1,12 +1,31 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: SuperViewController {
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Sample")
     }
-
-
 }
 
+extension ViewController {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Tap \(indexPath.row)!")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Sample", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
