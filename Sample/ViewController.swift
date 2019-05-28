7a03ef8880a72ca3c4e992353e1b25898e089969
maguhiro <maguhiro@gmail.com>
2019-05-28 14:22:16 +0900

    コードフォーマット適用

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -1,32 +1,32 @@
 import UIKit
 
 class ViewController: SuperViewController {
-    @IBOutlet var tableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Sample")
-    }
+  @IBOutlet var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Sample")
+  }
 }
 
 extension ViewController {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Tap \(indexPath.row)!")
-        present(SecondCollectionViewController(nibName: "SecondCollectionViewController", bundle: nil), animated: true)
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("Tap \(indexPath.row)!")
+    present(SecondCollectionViewController(nibName: "SecondCollectionViewController", bundle: nil), animated: true)
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
 
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Sample", for: indexPath)
-        cell.textLabel?.text = "\(indexPath.row)"
-        return cell
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Sample", for: indexPath)
+    cell.textLabel?.text = "\(indexPath.row)"
+    return cell
+  }
 }
